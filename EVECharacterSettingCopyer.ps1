e336aff5ec4fbd29b978a54bcbc4ff4b30c3a4f5
creatorlxd <1461637345@qq.com>
2025-06-13 20:19:14 +0800

    fix : add dir in argument

--- EVECharacterSettingCopyer.ps1
+++ EVECharacterSettingCopyer.ps1
@@ -1,6 +1,8 @@
 param (
 	[string]$source,
-	[string]$targetList
+	[string]$targetList,
+	[string]$sourceDirectory,
+	[string]$targetDirectory = $sourceDirectory
 )
 
 function Get-CharacterIdByName {
@@ -28,33 +30,40 @@ function Get-CharacterIdByName {
 	}
 }
 
-$targets = $targetList -split ","
+$targets = $targetList -split "[,;\s]+" | Where-Object { $_ -ne "" }
 Write-Host "Source Character: $source"
 Write-Host "Target Characters: $($targets -join ', ')"
-$localAppData = [System.Environment]::GetFolderPath('LocalApplicationData')
-$eveConfigRootPath = Join-Path -Path $localAppData -ChildPath "CCP/EVE"
-$characterConfigFileMap = @{}
-Get-ChildItem -Path $eveConfigRootPath -Filter "core_char_*.dat" -Recurse | ForEach-Object {
+# $localAppData = [System.Environment]::GetFolderPath('LocalApplicationData')
+# $eveConfigRootPath = Join-Path -Path $localAppData -ChildPath "CCP/EVE"
+$sourceCharacterConfigFileMap = @{}
+$targetCharacterConfigFileMap = @{}
+Get-ChildItem -Path $sourceDirectory -Filter "core_char_*.dat" -Recurse | ForEach-Object {
 	if ($_ -match 'core_char_(\d+).dat') {
 		$charId = [long]$matches[1]
-		$characterConfigFileMap[$charId] = $_.FullName
+		$sourceCharacterConfigFileMap[$charId] = $_.FullName
+	}
+}
+Get-ChildItem -Path $targetDirectory -Filter "core_char_*.dat" -Recurse | ForEach-Object {
+	if ($_ -match 'core_char_(\d+).dat') {
+		$charId = [long]$matches[1]
+		$targetCharacterConfigFileMap[$charId] = $_.FullName
 	}
 }
 $sourceCharacterId = Get-CharacterIdByName -name $source
 foreach ($targetCharacterName in $targets) {
 	$targetCharacterId = Get-CharacterIdByName -name $targetCharacterName
-	if ($targetCharacterId -and $characterConfigFileMap.ContainsKey($sourceCharacterId) -and $characterConfigFileMap.ContainsKey($targetCharacterId)) {
-		$sourceFile = $characterConfigFileMap[$sourceCharacterId]
-		$targetFile = $characterConfigFileMap[$targetCharacterId]
+	if ($targetCharacterId -and $sourceCharacterConfigFileMap.ContainsKey($sourceCharacterId) -and $targetCharacterConfigFileMap.ContainsKey($targetCharacterId)) {
+		$sourceFile = $sourceCharacterConfigFileMap[$sourceCharacterId]
+		$targetFile = $targetCharacterConfigFileMap[$targetCharacterId]
 		Copy-Item -Path $sourceFile -Destination $targetFile -Force
 		Write-Host "Copied settings from $source to $targetCharacterName"
 	}
 	else {
 		Write-Error "Could not find configuration files for $source or $targetCharacterName"
-		if (-not $characterConfigFileMap.ContainsKey($sourceCharacterId)) {
+		if (-not $sourceCharacterConfigFileMap.ContainsKey($sourceCharacterId)) {
 			Write-Error "Source character configuration file not found with ${sourceCharacterId}."
 		}
-		if (-not $characterConfigFileMap.ContainsKey($targetCharacterId)) {
+		if (-not $targetCharacterConfigFileMap.ContainsKey($targetCharacterId)) {
 			Write-Error "Target character configuration file not found with ${targetCharacterId}."
 		}
 	}
